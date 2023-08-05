dc0d3bbce4e024e5f85b8e89074e6ef447e70369
kingseungwooya <ksw_0505@nate.com>
2023-08-05 18:08:14 +0900

    feat: 회원정보 수정을 위한 기능

--- src/main/java/daehakro/server/domain/member/controller/MemberController.java
+++ src/main/java/daehakro/server/domain/member/controller/MemberController.java
@@ -1,20 +1,26 @@
 package daehakro.server.domain.member.controller;
 
 import daehakro.server.domain.member.controller.dto.request.CheckMemberDto;
+import daehakro.server.domain.member.controller.dto.request.EditDto;
 import daehakro.server.domain.member.controller.dto.request.KakaoLoginDto;
 import daehakro.server.domain.member.controller.dto.request.UserInfoDto;
 import daehakro.server.domain.member.controller.dto.response.SignUpValidationDto;
 import daehakro.server.domain.member.mail.dto.request.EmailAuthRequestDto;
+import daehakro.server.domain.member.repo.MemberRepository;
 import daehakro.server.domain.member.service.MemberService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -51,6 +57,23 @@ public class MemberController {
     @PostMapping("/auth-check")
     public ResponseEntity<SignUpValidationDto> authValidate(@RequestBody CheckMemberDto checkMemberDto) {
         return new ResponseEntity<>(memberService.validateUserAuth(checkMemberDto), HttpStatus.OK);
+    }
+
+    /**
+     * 회원 정보 수정
+     */
+    @PutMapping("/user-info")
+    public ResponseEntity<?> editUserInfo(@RequestBody EditDto editDto) {
+        memberService.editInfo(editDto);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @Autowired
+    MemberRepository memberRepository;
+    @DeleteMapping("/delete")
+    public void deleteMember(@RequestParam String memberId) {
+        memberRepository.deleteById(memberId);
     }
 
 }
